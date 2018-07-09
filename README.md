78727b59eca919a17e327e10c7e7d53f5047b62a
ahtee <benottedev@gmail.com>
2018-07-09 11:59:07 -0400

    Added bootstrap styles and intro stuff

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Mozart
 
 Find more information about [Mozart container orchestration](https://github.com/zbblanton/Mozart) tool.
+
+## Installing
+
+- Clone the repo to your local machine using git: `git clone https://github.com/ahtee/mozart-web.git`
+- `cd mozart-web`
+
+Run the code locally and open a web browser
+```
+npm start
+```
--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Jumbotron, Button } from 'react-bootstrap';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="jumbotron">
+          <h1 className="display-3">Mozart</h1>
+          <p class="lead">Container orchestration in Go</p>
+          <button type="button" className="btn btn-success">Get Started</button>
+        </div>
+
+        <div className="container">
+        <div className="row">
+
+          <div class="card col-md-4 col-sm-12">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+            </div>
+          </div>
+
+          <div class="card col-md-4 col-sm-12">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+            </div>
+          </div>
+
+          <div class="card col-md-4 col-sm-12">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+            </div>
+          </div>
+
+        </div>
       </div>
+
+      <footer class="footer">
+        <div class="container">
+          <span class="text-muted">Place sticky footer content here.</span>
+        </div>
+      </footer>
+    </div> // App.js
     );
   }
 }
