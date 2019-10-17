4be9a8aa11e0a649993ca720923fdfc71ce9b810
212615385 <Benjamin.Otte@ge.com>
2019-10-17 07:08:30 -0400

    adding basename to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/mozart">
       <Navigation title="Mozart" />
       <Switch>
         <Route exact path="/" component={Home} />
